88ea4a23ae97a693b382564c69a536095f3d7b6d
Johann <johann@latitude.ubuntu>
2017-03-22 00:10:14 +0100

    Add hysteresis for both cooling and heating

--- src/temp.ino
+++ src/temp.ino
@@ -5,32 +5,40 @@ int reading;
 int tempPin = A0;
 
 #define DIVTEMP 9.31
-#define CYCLE 50
+#define DELAY 5000
+#define THRESHOLD 1
+
 const int relaySignalPin = 2;
-int incr = 0;
+enum heatingStates { Heating, Cooling };
+enum heatingStates state;
+long unsigned timer;
 
 void setup() {
   analogReference(INTERNAL);
   Serial.begin(9600);
   pinMode(relaySignalPin, OUTPUT);
+  state = Heating;
+  timer = millis();
 }
 
 void loop() {
   reading = analogRead(tempPin);
   float tempRead = convertToTemp(reading);
   // Serial.println(convertToTemp(reading));
-  int tempTarget = 20;
-  if (incr >= CYCLE){
-    incr = 0;
-
-    if (tempRead < tempTarget) {
+  int tempTarget = 21;
+  if(millis() > timer + DELAY){
+    timer = millis();
+    if (state == Heating && tempRead < tempTarget + THRESHOLD) {
       startHeating();
-    } else {
+    } else if (state == Heating && tempRead > tempTarget + THRESHOLD) {
       stopHeating();
+    } else if (state == Cooling && tempRead > tempTarget - THRESHOLD) {
+      stopHeating();
+    } else {
+      startHeating();
     }
     Serial.println(convertToTemp(reading));
   }
-  incr++;
   delay(100);
 }
 
@@ -38,9 +46,11 @@ float convertToTemp(float reading) { return reading / DIVTEMP; }
 
 void startHeating() {
   Serial.print(" Heating... ");
+  state = Heating;
   digitalWrite(relaySignalPin, LOW);
 }
 void stopHeating() {
   Serial.print(" Cooling... ");
+  state = Cooling;
   digitalWrite(relaySignalPin, HIGH);
 }
